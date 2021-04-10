a7b339d8bbb4b28c3e16ea6e95e30aeebf7ae7b2
mdmarufsarker <mdmarufsarker2021@gmail.com>
2021-04-10 15:41:26 +0600

    SoloLearn Python

---
+++ Python/simple_calculator.py
@@ -0,0 +1,18 @@
+# Simple Calculator
+
+
+# Write a program to take two integers as input and output their sum.
+
+# Sample Input:
+# 2
+# 8
+
+# Sample Output:
+# 10
+# Remember, input() results in a string.
+
+
+num1 = int(input())
+num2 = int(input())
+sum = num1 + num2
+print(sum)
